16e6c62bde4aae150df987de6dbbb98ad57d85d0
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-27 16:57:53 -0300

    variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,12 @@
 variable "region" {
-
-  default = ""
-
+  default = "us-east-1"
+}
+variable "public_key" {
+  default = "us-east-1"
+}
+variable "private_key" {
+  default = "us-east-1"
+}
+variable "key_name" {
+  default = "us-east-1"
 }
