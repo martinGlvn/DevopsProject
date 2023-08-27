ee7ae73b33acf11f9bc9dbfc6f478028afe5cb15
martinGlvn <lkn.galvan.martin@gmail.com>
2023-08-27 16:53:39 -0300

    Instance_AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">4.0"
+      source  = "hashicorp/aws"
+      version = ">4.0"
     }
   }
   backend "s3" {
@@ -10,20 +10,86 @@ terraform {
   }
 }
 
-# 
-provider "aws"{
-    region = var.region
+# AWS provider
+provider "aws" {
+  region = var.region
 }
 
+# Config Resources EC2 =>
+resource "aws_instance" "server" {
+  ami                    = ami-052efd3df9dad4825 # Ubuntu Server 22.04 LTS - HVM
+  instance_type          = t2.micro
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.maingroup.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
+  tags = {
+    "name" = "DeployVM"
+  }
+}
 
-resource "aws_instance" "name"{
-    ami = ami-052efd3df9dad4825 # Ubuntu Server 22.04 LTS - HVM
-    instance_type = t2.micro
-    connection {
-        type = "ssh"
-        host = self.public_ip
-        user = "ubuntu"
-        private_key = var.private_key
-        timeout = "4m"
+# IAM =>
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-ECR-AUTH"
+}
+
+# Instance Security Group = ssh - http =>
+resource "aws_security_group" "maingroup" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
     }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+
+  ]
+}
+
+# key pairs =>
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+# =>
+output "instance_public_ip" {
+  value     = aws.instance.server.public_ip
+  sensitive = true
 }
